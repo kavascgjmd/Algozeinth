a3a42d85a28886457785106b0853fe30eee3604a
Ashutosh Shivhare <20je0209@cve.iitism.ac.in>
2023-03-27 02:45:50 +0530

    commit-2

---
+++ algozeinth/src/Components/Index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header'
+import './style.css'
+function Index() {
+   return (
+  <div>
+  <Header></Header>
+  </div>  
+  )
+}
+
+export default Index
